af766fe612e2cdeb19961e301a900771fb2fd859
asdfvar <asdfvar@gmail.com>
2016-07-24 17:11:56 -0700

    Incremental updates to the map files

--- simu_world/map/map.cpp
+++ simu_world/map/map.cpp
@@ -1,12 +1,25 @@
 #include "map.h"
+#include <cstdlib>
 
 Map::Map(unsigned int id_in)
 {
-   map_dim         = 64;
-   blocks          = new char[map_dim * map_dim * map_dim];
-   characteristics = new char[map_dim * map_dim * map_dim];
+   map_dim[0]      = 65;
+   map_dim[1]      = 65;
+   map_dim[2]      = 65;
+   blocks          = new char[map_dim[0] * map_dim[1] * map_dim[2]];
+   characteristics = new char[map_dim[0] * map_dim[1] * map_dim[2]];
 
    id = id_in;
+
+   create_random();
+}
+
+void Map::create_random( void)
+{
+   for (int ind = 0; ind < map_dim[0] * map_dim[1] * map_dim[2]; ind++) {
+      blocks[ind] = static_cast<char>( std::rand() % 2);
+      characteristics[ind] = 0;
+   }
 }
 
 Map::~Map(void)
@@ -18,9 +31,17 @@ Map::~Map(void)
 Map_grid::Map_grid(void)
 {
 
-   local_grid_size[0] = 11;
-   local_grid_size[1] = 11;
-   local_grid_size[2] = 11;
+   local_grid_size[0] = 11; // must be odd
+   local_grid_size[1] = 11; // must be odd
+   local_grid_size[2] = 11; // must be odd
+
+   virtual_grid_id_x = new int[local_grid_size[0]];
+   virtual_grid_id_y = new int[local_grid_size[1]];
+   virtual_grid_id_z = new int[local_grid_size[2]];
+
+   for (int k = 0; k < local_grid_size[0]; k++) virtual_grid_id_x[k] = k;
+   for (int k = 0; k < local_grid_size[1]; k++) virtual_grid_id_y[k] = k;
+   for (int k = 0; k < local_grid_size[2]; k++) virtual_grid_id_z[k] = k;
 
    std::size_t total_grid_size = local_grid_size[0] * local_grid_size[1] * local_grid_size[2];
 
@@ -33,4 +54,31 @@ Map_grid::~Map_grid(void)
 {
    std::size_t total_grid_size = local_grid_size[0] * local_grid_size[1] * local_grid_size[2];
    maps.erase (maps.begin(), maps.begin() + total_grid_size);
+
+   delete[] virtual_grid_id_x;
+   delete[] virtual_grid_id_y;
+   delete[] virtual_grid_id_z;
 }
+
+/*
+** Circular shift the cube in the specified direction
+*/
+void Map_grid::shift( int x, int y, int z)
+{
+   for (int k = 0; k < local_grid_size[0]; k++) {
+      for (virtual_grid_id_x[k] -= x;
+           virtual_grid_id_x[k] < 0;
+           virtual_grid_id_x[k] += local_grid_size[0]) { /* N/A */};
+   }
+   for (int k = 0; k < local_grid_size[1]; k++) {
+      for (virtual_grid_id_y[k] -= y;
+           virtual_grid_id_y[k] < 0;
+           virtual_grid_id_y[k] += local_grid_size[1]) { /* N/A */};
+   }
+   for (int k = 0; k < local_grid_size[2]; k++) {
+      for (virtual_grid_id_z[k] -= z;
+           virtual_grid_id_z[k] < 0;
+           virtual_grid_id_z[k] += local_grid_size[2]) { /* N/A */};
+   }
+}
+
--- simu_world/map/map.h
+++ simu_world/map/map.h
@@ -55,8 +55,10 @@ class Map
       */
       char *characteristics;
 
-      unsigned int map_dim;
+      unsigned int map_dim[3];
       unsigned int id;
+
+      void create_random( void);
 
 };
 
@@ -66,8 +68,14 @@ class Map_grid
       Map_grid(void);
       ~Map_grid(void);
 
+      void shift( int x, int y, int z);
+
    private:
       std::size_t local_grid_size[3];
+
+      int *virtual_grid_id_x,
+          *virtual_grid_id_y,
+          *virtual_grid_id_z;
 
       std::vector<Map> maps;
 
