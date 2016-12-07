1cdfe39761966bb66ba82f79148746b0996fcffe
asdfvar <asdfvar@gmail.com>
2016-12-07 01:05:57 -0800

    Cleaning.

--- simu_world/opengl_interface.cpp
+++ simu_world/opengl_interface.cpp
@@ -128,37 +128,66 @@ void Opengl_interface::idle( void)
                              direction[1] * direction[1]);
 
       float rot_z[3][3];
-      rot_z[0][0] = direction[1] / norm_xy; rot_z[0][1] = -direction[0] / norm_xy; rot_z[0][2] = 0.0f;
-      rot_z[1][0] = direction[0] / norm_xy; rot_z[1][1] =  direction[1] / norm_xy; rot_z[1][2] = 0.0f;
-      rot_z[2][0] = 0.0f;                   rot_z[2][1] =  0.0f;                   rot_z[2][2] = 1.0f;
+
+      rot_z[0][0] =  direction[1] / norm_xy; rot_z[0][1]
+                  = -direction[0] / norm_xy; rot_z[0][2]
+                  = 0.0f;
+
+      rot_z[1][0] = direction[0] / norm_xy; rot_z[1][1]
+                  = direction[1] / norm_xy; rot_z[1][2]
+                  = 0.0f;
+
+      rot_z[2][0] = 0.0f;
+      rot_z[2][1] = 0.0f;
+      rot_z[2][2] = 1.0f;
 
       float rot_x[3][3];
+
       rot_x[0][0] = 1.0f; rot_x[0][1] =  0.0f; rot_x[0][2] =  0.0f;
       rot_x[1][0] = 0.0f; rot_x[1][1] = cos_y; rot_x[1][2] = -sin_y;
       rot_x[2][0] = 0.0f; rot_x[2][1] = sin_y; rot_x[2][2] =  cos_y;
 
       float rot_z_back[3][3];
+
       rot_z_back[0][0] = direction[1] / norm_xy; rot_z_back[0][1]
                        = direction[0] / norm_xy; rot_z_back[0][2]
                        = 0.0f;
+
       rot_z_back[1][0] = -direction[0] / norm_xy; rot_z_back[1][1]
                        = direction[1] / norm_xy; rot_z_back[1][2]
                        = 0.0f;
+
       rot_z_back[2][0] = 0.0f;
       rot_z_back[2][1] = 0.0f;
       rot_z_back[2][2] = 1.0f;
 
-      temp[0] = direction[0] * rot_z[0][0] + direction[1] * rot_z[0][1] + direction[2] * rot_z[0][2];
-      temp[1] = direction[0] * rot_z[1][0] + direction[1] * rot_z[1][1] + direction[2] * rot_z[1][2];
-      temp[2] = direction[0] * rot_z[2][0] + direction[1] * rot_z[2][1] + direction[2] * rot_z[2][2];
+      temp[0] = direction[0] * rot_z[0][0] +
+                direction[1] * rot_z[0][1] +
+                direction[2] * rot_z[0][2];
+
+      temp[1] = direction[0] * rot_z[1][0] +
+                direction[1] * rot_z[1][1] +
+                direction[2] * rot_z[1][2];
+
+      temp[2] = direction[0] * rot_z[2][0] +
+                direction[1] * rot_z[2][1] +
+                direction[2] * rot_z[2][2];
 
       direction[0] = temp[0];
       direction[1] = temp[1];
       direction[2] = temp[2];
 
-      temp[0] = direction[0] * rot_x[0][0] + direction[1] * rot_x[0][1] + direction[2] * rot_x[0][2];
-      temp[1] = direction[0] * rot_x[1][0] + direction[1] * rot_x[1][1] + direction[2] * rot_x[1][2];
-      temp[2] = direction[0] * rot_x[2][0] + direction[1] * rot_x[2][1] + direction[2] * rot_x[2][2];
+      temp[0] = direction[0] * rot_x[0][0] +
+                direction[1] * rot_x[0][1] +
+                direction[2] * rot_x[0][2];
+
+      temp[1] = direction[0] * rot_x[1][0] +
+                direction[1] * rot_x[1][1] +
+                direction[2] * rot_x[1][2];
+
+      temp[2] = direction[0] * rot_x[2][0] +
+                direction[1] * rot_x[2][1] +
+                direction[2] * rot_x[2][2];
 
       direction[0] = temp[0];
       direction[1] = temp[1];
