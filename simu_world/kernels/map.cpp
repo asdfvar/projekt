2f04925ec9533396f4a7728375426a9e0e6dc315
asdfvar <asdfvar@gmail.com>
2016-12-17 23:20:23 -0800

    updates

--- simu_world/kernels/map.cpp
+++ simu_world/kernels/map.cpp
@@ -34,7 +34,7 @@ void Map::create_random( void)
 {
    for (int ind = 0; ind < map_dim[0] * map_dim[1] * map_dim[2]; ind++)
    {
-      int random_num = std::rand() % 100;
+      int random_num = std::rand() % 400;
       if (random_num == 1)
          blocks[ind] = 1;
       else
