c699a5e9ebe700f0e5e0f8caa80bd83a2cd53e68
asdfvar <asdfvar@gmail.com>
2016-12-28 16:25:58 -0800

    Cleanup

--- simu_world/kernels/draw_scene.cpp
+++ simu_world/kernels/draw_scene.cpp
@@ -12,8 +12,7 @@
 ** The coordinates are converted so that they are relative
 ** to the user.
 */
-void draw_scene( User                  *user,
-                 Map_grid              *map_grid)
+void draw_scene( User *user, Map_grid *map_grid)
 {
 
    map_grid->render_chunk( user);
