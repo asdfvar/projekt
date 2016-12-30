930354ee83dcc0428b5b96617dc8c031b8a97c91
asdfvar <asdfvar@gmail.com>
2016-12-29 22:45:41 -0800

    extracted the chunk stuff into its own set of files from the mapping stuff

--- simu_world/kernels/map.h
+++ simu_world/kernels/map.h
@@ -1,41 +1,11 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include "chunk.h"
 #include <iostream>
 #include <vector>
 #include "opengl_interface.h"
 #include "user.h"
-
-/*
-** class name: Chunk
-*/
-class Chunk
-{
-   public:
-      Chunk( void);
-      Chunk( unsigned int id_in, int *chunk_dim_in, float *position_in);
-      ~Chunk( void );
-
-      // Functions
-      int  get_position(float *position_out, int block_index);
-      void change(float *position_in);
-      unsigned int get_dimensions(void);
-      void display_info( void);
-      bool position_in_chunk( float *poisition_in);
-      void move( int x, int y, int z);
-
-   private:
-
-      // Parameters:
-      int         *blocks;
-      unsigned int chunk_dim[3];
-      unsigned int id;
-      float        position[3];
-
-      // Functions:
-      void create_random( void);
-
-};
 
 /*
 ** class name: Map
