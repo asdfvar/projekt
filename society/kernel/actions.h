4d6988ba4ee024651e90c11e4048709508d3bf65
asdfvar <asdfvar@gmail.com>
2020-01-16 21:57:55 -0800

    start of writing custom list. additions to CR list

--- society/kernel/actions.h
+++ society/kernel/actions.h
@@ -27,4 +27,83 @@ class Action
       bool  complete;        // flag to determine if the action is complete
 };
 
+class Node
+{
+   public:
+
+      Action *action;
+      Node   *next;
+      Node   *previous;
+};
+
+class Actions_container
+{
+   public:
+
+      Actions_container (void)
+      {
+         container_size       = 0;
+         current_action_index = 0;
+      }
+
+      // insert action (index)
+      void insert_action (Action *action, int index)
+      {
+         // advance to the desired action node index
+         while (current_action_index < index)
+         {
+            current_action_node = current_action_node->next;
+            current_action_index++;
+         }
+
+         while (current_action_index > index)
+         {
+            current_action_node = current_action_node->previous;
+            current_action_index--;
+         }
+
+         // TODO: logic to insert
+      }
+
+      // access action (index)
+
+      // pop action (index) // returns address to the select action and removes it from the list
+      Action *pop (int index)
+      {
+         if (container_size == 0) return nullptr;
+
+         // advance to the desired action node index
+         while (current_action_index < index)
+         {
+            current_action_node = current_action_node->next;
+            current_action_index++;
+         }
+
+         while (current_action_index > index)
+         {
+            current_action_node = current_action_node->previous;
+            current_action_index--;
+         }
+
+         Action *select_action = current_action_node->action;
+
+         // Effectively pop the node off
+         Node *next_node = current_action_node->next;
+         current_action_node = current_action_node->previous;
+         current_action_node->next = next_node;
+
+         container_size--;
+
+         return select_action;
+      }
+
+   private:
+
+      int container_size;
+      // first index pointer
+
+      int   current_action_index;
+      Node *current_action_node;
+};
+
 #endif
