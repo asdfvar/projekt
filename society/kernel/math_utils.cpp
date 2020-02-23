9148203c689d63db5fe14ce4fdf4dcfb802b5a0f
asdfvar <asdfvar@gmail.com>
2020-02-22 17:20:30 -0800

    resolved bugs with perlin noise. not quite right but close enough

--- society/kernel/math_utils.cpp
+++ society/kernel/math_utils.cpp
@@ -14,7 +14,7 @@ void rref (float *matrix, int num_rows, int num_cols)
       // Swap rows if the leading element is near zero
       for (int row = leading_element + 1; row < num_rows; row++)
       {
-         if ( matrix[row * num_cols + leading_element] > matrix[leading_element * num_cols + leading_element])
+         if (matrix[row * num_cols + leading_element] > matrix[leading_element * num_cols + leading_element])
             for (int col = leading_element; col < num_cols; col++)
             {
                float temp = matrix[leading_element * num_cols + col];
@@ -25,16 +25,10 @@ void rref (float *matrix, int num_rows, int num_cols)
          leading_element_value = matrix[leading_element * num_cols + leading_element];
       }
 
-// <<< left off here (stability fix)
-      leading_element_value = matrix[leading_element * num_cols + leading_element];
+      float leading_element_value_inv = 1.0f / matrix[leading_element * num_cols + leading_element];
 
       for (int col = leading_element; col < num_cols; col++)
-      {
-         matrix[leading_element * num_cols + col] /= leading_element_value;
-      }
-
-      leading_element_value = matrix[leading_element * num_cols + leading_element];
-// >>> left off here (stability fix)
+         matrix[leading_element * num_cols + col] *= leading_element_value_inv;
 
       for (int row = 0; row < num_rows; row++)
       {
@@ -46,8 +40,8 @@ void rref (float *matrix, int num_rows, int num_cols)
          for (int col = 0; col < num_cols; col++)
          {
             matrix[row * num_cols + col] =
-               leading_element_value * matrix[row             * num_cols + col] -
-               row_factor            * matrix[leading_element * num_cols + col];
+               matrix[row * num_cols + col] -
+               row_factor * matrix[leading_element * num_cols + col];
          }
       }
    }
@@ -82,62 +76,59 @@ void perlin (
    float *gradient[2];
 
    grid_cells[0] = (int*)buffer;
-   grid_cells[1] = grid_cells[0]         + (num_grid_cells[0] + 1) * (num_grid_cells[1] + 1);
-   gradient[0]   = (float*)grid_cells[1] + (num_grid_cells[0] + 1) * (num_grid_cells[1] + 1);
-   gradient[1]   = gradient[0]           + (num_grid_cells[0] + 1) * (num_grid_cells[1] + 1);
+   grid_cells[1] = grid_cells[0]         + (num_grid_cells[0] + 2) * (num_grid_cells[1] + 2);
+   gradient[0]   = (float*)grid_cells[1] + (num_grid_cells[0] + 2) * (num_grid_cells[1] + 2);
+   gradient[1]   = gradient[0]           + (num_grid_cells[0] + 2) * (num_grid_cells[1] + 2);
 
-   for (int indy = 0, ind = 0; indy <= num_grid_cells[0]; indy++)
+   float *top_gradients[2];
+   top_gradients[0]    = new float[num_grid_cells[0] + 1];
+   top_gradients[1]    = new float[num_grid_cells[0] + 1];
+
+   float *bottom_gradients[2];
+   bottom_gradients[0] = new float[num_grid_cells[0] + 1];
+   bottom_gradients[1] = new float[num_grid_cells[0] + 1];
+
+   for (int indx = 0; indx <= num_grid_cells[0]; indx++)
    {
-      for (int indx = 0; indx <= num_grid_cells[0]; indx++, ind++)
-      {
-         grid_cells[0][ind] = indx * size[0] / num_grid_cells[0];
-         grid_cells[1][ind] = indy * size[1] / num_grid_cells[1];
-
-         gradient[0][ind] = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
-         gradient[1][ind] = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
-      }
+      top_gradients[0][indx]    = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
+      top_gradients[1][indx]    = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
+      bottom_gradients[0][indx] = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
+      bottom_gradients[1][indx] = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
    }
 
-   int step[2] = {
-      size[0] / num_grid_cells[0],
-      size[1] / num_grid_cells[1] };
+   int step[2] = { size[0] / num_grid_cells[0], size[1] / num_grid_cells[1] };
 
    for (int iy = 0, ind = 0; iy < size[1]; iy++)
    {
       for (int ix = 0; ix < size[0]; ix++, ind++)
       {
-         int grid_ind_ll = (iy / step[1]    ) * (num_grid_cells[0] + 1) + (ix / step[0]);
-         int grid_ind_lr = (iy / step[1]    ) * (num_grid_cells[0] + 1) + (ix / step[0]) + 1;
-         int grid_ind_ul = (iy / step[1] + 1) * (num_grid_cells[0] + 1) + (ix / step[0]);
-         int grid_ind_ur = (iy / step[1] + 1) * (num_grid_cells[0] + 1) + (ix / step[0]) + 1;
+         int cell[2] = { ix / step[0], iy / step[1] };
 
-         int   grid_cell_ll[2] = { grid_cells[0][grid_ind_ll], grid_cells[1][grid_ind_ll] };
-         float gradient_ll[2]  = { gradient  [0][grid_ind_ll], gradient  [1][grid_ind_ll] };
-
-         int   grid_cell_lr[2] = { grid_cells[0][grid_ind_lr], grid_cells[1][grid_ind_lr] };
-         float gradient_lr[2]  = { gradient  [0][grid_ind_lr], gradient  [1][grid_ind_lr] };
-
-         int   grid_cell_ul[2] = { grid_cells[0][grid_ind_ul], grid_cells[1][grid_ind_ul] };
-         float gradient_ul[2]  = { gradient  [0][grid_ind_ul], gradient  [1][grid_ind_ul] };
-
-         int   grid_cell_ur[2] = { grid_cells[0][grid_ind_ur], grid_cells[1][grid_ind_ur] };
-         float gradient_ur[2]  = { gradient  [0][grid_ind_ur], gradient  [1][grid_ind_ur] };
+         float center[2] = { 0.5f * step[0], 0.5f * step[1] };
 
          float dotp_ll =
-            (float)(ix - grid_cell_ll[0]) * gradient_ll[0] +
-            (float)(iy - grid_cell_ll[1]) * gradient_ll[1];
+            (float)(ix - cell[0] * step[0])       * bottom_gradients[0][cell[0]] +
+            (float)(iy - cell[1] * step[1])       * bottom_gradients[1][cell[0]];
+
+dotp_ll = bottom_gradients[0][cell[0]];
 
          float dotp_lr =
-            (float)(ix - grid_cell_lr[0]) * gradient_lr[0] +
-            (float)(iy - grid_cell_lr[1]) * gradient_lr[1];
+            (float)(ix - (cell[0] + 1) * step[0]) * bottom_gradients[0][cell[0]+1] +
+            (float)(iy - cell[1] * step[1])       * bottom_gradients[1][cell[0]];
+
+dotp_lr = bottom_gradients[0][cell[0]+1];
 
          float dotp_ul =
-            (float)(ix - grid_cell_ul[0]) * gradient_ul[0] +
-            (float)(iy - grid_cell_ul[1]) * gradient_ul[1];
+            (float)(ix - cell[0] * step[0])       * top_gradients[0][cell[0]] +
+            (float)(iy - (cell[1] + 1) * step[1]) * top_gradients[1][cell[0]+1];
+
+dotp_ul = top_gradients[0][cell[0]];
 
          float dotp_ur =
-            (float)(ix - grid_cell_ur[0]) * gradient_ur[0] +
-            (float)(iy - grid_cell_ur[1]) * gradient_ur[1];
+            (float)(ix - (cell[0] + 1) * step[0]) * top_gradients[0][cell[0]+1] +
+            (float)(iy - (cell[1] + 1) * step[1]) * top_gradients[1][cell[0]+1];
+
+dotp_ur = top_gradients[0][cell[0]+1];
 
          // 2D bilinear interpolation on an arbitrary grid can be
          // obtained by solving for the parameters A, B, C, D s.t.
@@ -157,23 +148,14 @@ void perlin (
          // dotp_ur = A * ur_0 * ur_1 + B * ur_0 + C * ur_1 + D
          // dotp_ul = A * ul_0 * ul_1 + B * ul_0 + C * ul_1 + D
 
-         float ll_0 = (float)grid_cell_ll[0];
-         float ll_1 = (float)grid_cell_ll[1];
-         float lr_0 = (float)grid_cell_lr[0];
-         float lr_1 = (float)grid_cell_lr[1];
-         float ur_0 = (float)grid_cell_ur[0];
-         float ur_1 = (float)grid_cell_ur[1];
-         float ul_0 = (float)grid_cell_ul[0];
-         float ul_1 = (float)grid_cell_ul[1];
-
-         ll_0 -= (float)grid_cell_ll[0];
-         ll_1 -= (float)grid_cell_ll[1];
-         lr_0 -= (float)grid_cell_ll[0];
-         lr_1 -= (float)grid_cell_ll[1];
-         ur_0 -= (float)grid_cell_ll[0];
-         ur_1 -= (float)grid_cell_ll[1];
-         ul_0 -= (float)grid_cell_ll[0];
-         ul_1 -= (float)grid_cell_ll[1];
+         float ll_0 = 0.0f;
+         float ll_1 = 0.0f;
+         float lr_0 = step[0];
+         float lr_1 = 0.0f;
+         float ur_0 = step[0];
+         float ur_1 = step[1];
+         float ul_0 = 0.0f;
+         float ul_1 = step[1];
 
          float matrix[4 * 5] = {
             ll_0 * ll_1, ll_0, ll_1, 1.0f, dotp_ll,
@@ -181,39 +163,48 @@ void perlin (
             ur_0 * ur_1, ur_0, ur_1, 1.0f, dotp_ur,
             ul_0 * ul_1, ul_0, ul_1, 1.0f, dotp_ul };
 
-#if 1
-   std::cout << "matrix before RREF:" << std::endl;
-   for (int row = 0, ind = 0; row < 4; row++)
-   {
-      for (int col = 0; col < 5; col++, ind++)
-         std::cout << matrix[ind] << ", ";
-      std::cout << std::endl;
-   }
-   std::cout << std::endl;
-#endif
          // Reduced Row-Echelon form
          rref (matrix, 4, 5);
-#if 1
-   std::cout << "matrix after RREF:" << std::endl;
-   for (int row = 0, ind = 0; row < 4; row++)
-   {
-      for (int col = 0; col < 5; col++, ind++)
-         std::cout << matrix[ind] << ", ";
-      std::cout << std::endl;
-   }
-   std::cout << std::endl;
-#endif
 
          float A = matrix[4];
          float B = matrix[9];
          float C = matrix[14];
          float D = matrix[19];
 
-         float fix = (float)(ix - grid_cell_ll[0]);
-         float fiy = (float)(iy - grid_cell_ll[1]);
+         // map the fine cell to the equivalent fine cell in the coarse cell
+         float fix = (float)(ix - cell[0] * step[0]);
+         float fiy = (float)(iy - cell[1] * step[1]);
 
-         array[ind] = A * fix * fiy + B * fix + C * fiy + D;
+         array[ind] += A * fix * fiy + B * fix + C * fiy + D;
       }
+
+      if ((iy + 1) % step[1] == 0)
+      {
+         // Rotate the gradient buffers (push the top gradients to the bottom gradients)
+         for (int indx = 0; indx <= num_grid_cells[0]; indx++)
+         {
+            float *temp[2];
+
+            temp[0]             = bottom_gradients[0];
+            temp[1]             = bottom_gradients[1];
+            bottom_gradients[0] = top_gradients[0];
+            bottom_gradients[1] = top_gradients[1];
+            top_gradients[0]    = temp[0];
+            top_gradients[1]    = temp[1];
+         }
+
+         // Compute new top gradients
+         for (int indx = 0; indx <= num_grid_cells[0]; indx++)
+         {
+            top_gradients[0][indx] = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
+            top_gradients[1][indx] = ((float)(rand () % 2000) / 1000.0f - 1.0f) * scale;
+         }
+      }
+
    }
 
+   delete[] top_gradients[0];
+   delete[] top_gradients[1];
+   delete[] bottom_gradients[0];
+   delete[] bottom_gradients[1];
 }
--- society/kernel/test/perlin_noise/test.cpp
+++ society/kernel/test/perlin_noise/test.cpp
@@ -11,38 +11,23 @@ int main (void)
    float *buffer = new float[80000];
 
    int    size[2] = {100, 100};
-   float  scale = 1.0f;
+   float  scale = 4.0f;
    int    num_grid_cells[2] = {6, 6};
    float  min_grid_point_dist = 8.0f;
 
-   float *matrix = array;
+   for (int k = 0; k < size[0]*size[1]; k++) array[k] = 0.0f;
 
-   for (int ind = 0; ind < 4 * 5; ind++)
-      matrix[ind] = (float)(rand () % 100) / 100.0f;
+   perlin (
+         array,
+         size,
+         scale,
+         num_grid_cells,
+         min_grid_point_dist,
+         buffer);
 
-#if 0
-   std::cout << "matrix before RREF:" << std::endl;
-   for (int row = 0, ind = 0; row < 4; row++)
-   {
-      for (int col = 0; col < 5; col++, ind++)
-         std::cout << matrix[ind] << ", ";
-      std::cout << std::endl;
-   }
-   std::cout << std::endl;
-#endif
-
-   rref (matrix, 4, 5);
-
-#if 0
-   std::cout << "matrix after RREF:" << std::endl;
-   for (int row = 0, ind = 0; row < 4; row++)
-   {
-      for (int col = 0; col < 5; col++, ind++)
-         std::cout << matrix[ind] << ", ";
-      std::cout << std::endl;
-   }
-   std::cout << std::endl;
-#endif
+   num_grid_cells[0] = 10;
+   num_grid_cells[1] = 10;
+   scale = 1.0f;
 
    perlin (
          array,
@@ -53,20 +38,22 @@ int main (void)
          buffer);
 
 #if 1
-   std::ofstream asdf;
-   asdf.open ("output", std::ios::out);
+   std::ofstream output;
+   output.open ("output", std::ios::out);
+
    for (int row = 0, ind = 0; row < size[1]; row++)
    {
       for (int col = 0; col < size[0]; col++, ind++)
       {
-         asdf << std::setprecision(3) << array[ind];
-         if (col == size[0] - 1) asdf << "; ";
-         else asdf << ", ";
+         output << std::setprecision(3) << array[ind];
+         if (col == size[0] - 1) output << "; ";
+         else output << ", ";
       }
-      asdf << std::endl;
+
+      output << std::endl;
    }
-asdf << "stuff\n";
-   asdf.close ();
+
+   output.close ();
 #endif
 
    delete[] array;
